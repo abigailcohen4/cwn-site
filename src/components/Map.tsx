7ce4e128c8d2282620097a09251176dbcd574480
Abi <abigailcohen481@gmail.com>
2025-05-06 12:23:18 -0400

    styled components install and popup style moved

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "@arcgis/map-components/dist/components/arcgis-map"
 
 const Map = ( ) => {
--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,26 @@
-import React from 'react'
+import styled from "styled-components"
+
+const Styles = {
+    Popup: styled.div`{
+        position: absolute; 
+        left: 0; 
+        right: 0; 
+        margin-inline: auto; 
+        width: fit-content;
+      
+        border-style: solid; 
+        border-color: whitesmoke;
+        border-width: 10px;
+        background-color: whitesmoke;
+      
+        z-index: 10; 
+      
+        display: flex;
+        vertical-align: middle;
+        justify-content: center;
+        align-items: center;
+      }`
+}
 
 const closePopup = () => { 
     const pup = document.getElementById("Popup");
@@ -9,13 +31,13 @@ const closePopup = () => {
 
 const Popup = ( ) => {
     return(
-        <div className="popup" id="myPopup">
-          <span className="popuptext">
-          <h3>Welcome to the Community Wireless Network Simulator!</h3>
-          <p>Use this map to model potential community wireless networks. </p>
-          <button onClick={closePopup}> Close </button>
-          </span>
-        </div>
+        <Styles.Popup> 
+            <div id="myPopup">
+                <h3>Welcome to the Community Wireless Network Simulator!</h3>
+                <p>Use this map to model potential community wireless networks. </p>
+                <button onClick={closePopup}> Close </button>
+            </div>
+        </Styles.Popup>
     )
 }
 
