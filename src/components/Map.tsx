4706e9110a5ae5d5fd697ca1f86eee9315692a59
Abi <abigailcohen481@gmail.com>
2025-05-06 13:18:56 -0400

    fixed map style stuff and added lineofsightwidget

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,73 @@
-import "@arcgis/map-components/dist/components/arcgis-map"
+import React, { useEffect } from "react";
+import SceneView from "@arcgis/core/views/SceneView.js";
+import WebScene from "@arcgis/core/WebScene.js";
 
-const Map = ( ) => {
-    return(
-        <div className="map-container">
-        <arcgis-map item-id="066adf91ce554be9843fc6bea7611c10">
-            <arcgis-zoom position="top-left"></arcgis-zoom>
-            <arcgis-legend position="bottom-left"></arcgis-legend>
-            <arcgis-editor position="top-right"></arcgis-editor>
-        </arcgis-map>
-      </div>
-    )
-}
+import LineOfSight from "@arcgis/core/widgets/LineOfSight.js";
+import LineOfSightAnalysis from "@arcgis/core/analysis/LineOfSightAnalysis.js";
+import LineOfSightAnalysisObserver from "@arcgis/core/analysis/LineOfSightAnalysisObserver.js";
+import LineOfSightAnalysisTarget from "@arcgis/core/analysis/LineOfSightAnalysisTarget.js";
+import LineOfSightAnalysisView3D from "@arcgis/core/views/3d/analysis/LineOfSightAnalysisView3D.js";
+import styled from "styled-components";
 
-export default Map
+const Styles = {
+    Scene: styled.div`
+        .map-container {
+            width: 100vw;
+            height: 90vh;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    `
+};
+
+
+
+const Scene = () => {
+    useEffect(() => {
+        // Initialize the WebScene and SceneView after the component mounts
+        const scene = new WebScene({
+            portalItem: {
+                id: "4700fecae9b14f5ca640957514ce9d31"
+            }
+        });
+
+        const view = new SceneView({
+            map: scene,
+            container: "scene-container",
+            analyses: [ new LineOfSightAnalysis() ]
+        });
+
+        const lineOfSight = new LineOfSight({
+            view: view
+          });
+        
+          view.ui.add(lineOfSight, {
+            position: "bottom-left"
+          });
+
+        // Cleanup function to destroy the view when the component unmounts
+        return () => {
+            if (view) {
+                view.destroy();
+            }
+        };
+    }, []); // Empty dependency array ensures this runs only once after mount
+
+    // const async function analysisView = await view.whenAnalysisView(analysis)
+    //     .then(function(view) {
+    //     // The layerview for the layer
+    //     })
+    //     .catch(function(error) {
+    //     // An error occurred during the layerview creation
+    //     });
+
+    return (
+        <Styles.Scene>
+            <div id="scene-container" className="map-container"></div>
+        </Styles.Scene>
+    );
+};
+
+export default Scene;
