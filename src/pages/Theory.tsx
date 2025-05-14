4b478822c1fe04fe0191c3ee927ea993d4c2ea01
Abi <abigailcohen481@gmail.com>
2025-05-14 19:30:03 -0400

    added text to theory page

--- src/pages/Theory.tsx
+++ src/pages/Theory.tsx
@@ -2,9 +2,24 @@ function Theory() {
 
   return (
     <>
-      <div>
+      <div className="textPage">
         <h1>Theoretical Analysis</h1>
-        <p>Below will be an exploration of the social/implicit understandings behind CWN infrastructure.</p>
+        <p>Community Wireless Networks (CWNs) broaden internet access through an implementation of internet infrastructure alternative to that of traditional Internet Service Providers in both technical and social ways. Mechanisms of this infrastructure present democracy and proximity as essentials for internet access. These principles guide the mesh network topology most CWNs implement and demonstrate a potential for propagation into the social realm. 
+Through an application of Langdon Winner's theory of technological politics (1980), I will lay out potential social implications of CWNs. As Winner asserts, technology materializes a specific relationship to power. Technological politics may take on one of two forms: the technology being a method of resolving an issue or the technology being particularly compatible with a power system (Ibid, 123). The first form asserts that modern machines may be implemented to enhance or enforce a privilege system. A machine's design enables this by producing outcomes logically related to its execution (Ibid, 125). The second form describes technology that grants a “justification for authority,” as it operates best with a specific power structure (Ibid, 130).  
+In this explanation, I will inspect artifacts, physical objects or features with a specific purpose, and techniques, abstract methods whose manifested implementation may vary (Dafoe 2015, 1051). In this exploration, CWNs' artifacts subscribe to the first form, while its techniques subscribe to the second. 
+</p> 
+<h2> THE TECH </h2>
+<h3>Artifacts </h3>
+<ol> 
+  <li><strong>IEEE 802.11 (Wi-Fi) Protocol and Line of Sight. </strong>Alternatively to predominant wired internet infrastructure, CWNs implement a wireless network to connect their community. To construct a wireless network, routers are strategically placed to enable a line of sight between each other. 
+  One's signal will be stronger the more routers within line of sight, but one only needs a connection to one router to connect to the network. A wireless router uses IEEE 802.11 (Wi-Fi) protocol to transmit data to all nodes within its scope. Any node with an unobstructed view will receive its signal. 
+  With this artifact, CWNs transform areas of inadequate internet service to a web of invisible connections. Applying LOS to this problem communicates the significance of community interdependence. Speaking and listening to individuals in proximity, as their routers do, can solve a community-wide problem and create a greater force. 
+  A wireless network may inspire interpersonal networks to take their shape. </li>
+  <li><strong>Power over Ethernet. </strong>Routers are installed along the wall or on the rooftop of buildings to enable line of sight. This placement complicates the issue of powering a router. In response, CWNs like Philly Community Wireless and NYC Mesh commonly use routers able to be powered over ethernet ports (cite their docs!). 
+  Ethernet was designed to support wired Internet access. At its release, the only way to access the Internet was plugging an ethernet cable from the ethernet port to one's computer. With the widespread adoption of Wi-Fi, ethernet ports' necessity has significantly declined, leaving many ports unused. 
+  In 2000, Power over Ethernet introduced a mechanism to deliver DC power over Category 5 cable. This feature transformed an increasingly obsolete hardware to a 
+</li>
+</ol>
       </div>
     </>
   )
