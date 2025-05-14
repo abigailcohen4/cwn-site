b66ba4e12fe6ee30ae7d6127a4e017f72bf04ac1
Abi <abigailcohen481@gmail.com>
2025-05-14 19:29:49 -0400

    removed styles from About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,11 @@
-import styled from "styled-components"
-const Styles = {
-    Theory: styled.div`
-        margin-left: 20vw;
-        margin-right: 20vw;
-
-    h1{
-        color: white;
-        text-align: center; 
-        }`
-    
-}
 function About() {
     return (
-      <Styles.Theory>
-        <div>
+        <div className="textPage">
           <h1>What if internet infrastructure could be managed by people from your own community?           </h1>
           <p>In places where Internet access is too expensive, unreliable, or simply nonexistent,
             individuals have stepped up to form their own local networks. My research looked at how these networks 
             typically operate ie the devices and other infrastructure they use to provide internet and what that could mean about our values.</p>
         </div>
-      </Styles.Theory>
     )
   }
   
